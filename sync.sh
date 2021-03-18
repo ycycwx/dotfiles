8606156a9e4e7186001571d2b59102d387c7cbc6
yangchenyi <yangchenyi@baidu.com>
2021-03-19 01:36:54 +0800

    refactor

---
+++ sync.sh
@@ -0,0 +1,23 @@
+BACKUP=./backup
+
+# homebrew
+brew bundle dump --force --file $BACKUP/Brewfile
+
+# aria2
+mkdir -p $BACKUP/aria2
+cp -r $HOME/.aria2/* $BACKUP/aria2/
+
+# prettier
+cp $HOME/.prettierrc $BACKUP/prettierrc
+
+# vimrc
+cp $HOME/.vimrc $BACKUP/vimrc
+
+# zshrc
+cp $HOME/.zshrc $BACKUP/zshrc
+
+# tmux
+cp $HOME/.tmux.conf $BACKUP/tmux.conf
+
+# git
+cp -r $HOME/.gitconfig $BACKUP/gitconfig
