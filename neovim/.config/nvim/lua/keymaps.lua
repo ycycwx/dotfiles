ff7093c863311ff4e8bf86ace027be7d6b3aed5b
ycycwx <yytcjcy@gmail.com>
2022-01-11 02:31:32 +0800

    refactor: aadd neovim folder for stow

---
+++ neovim/.config/nvim/lua/keymaps.lua
@@ -0,0 +1,53 @@
+-----------------------------------------------------------
+-- Keymaps configuration file: keymaps of neovim
+-- and plugins.
+-----------------------------------------------------------
+
+local map = vim.api.nvim_set_keymap
+local default_opts = { noremap = true, silent = true }
+-- local cmd = vim.cmd
+
+-----------------------------------------------------------
+-- Neovim shortcuts:
+-----------------------------------------------------------
+
+-- clear search highlighting
+map("n", "<leader>c", ":nohl<CR>", default_opts)
+
+-- map Esc to jk
+map("i", "jk", "<Esc>", { noremap = true })
+
+-- don't use arrow keys
+map("", "<up>", "<nop>", { noremap = true })
+map("", "<down>", "<nop>", { noremap = true })
+map("", "<left>", "<nop>", { noremap = true })
+map("", "<right>", "<nop>", { noremap = true })
+
+-- fast saving with <leader> and s
+map("n", "<leader>s", ":w<CR>", default_opts)
+map("i", "<leader>s", "<C-c>:w<CR>", default_opts)
+
+-- C-j C-k
+map("n", "<C-j>", "5j", default_opts)
+map("n", "<C-k>", "5k", default_opts)
+map("v", "<C-j>", "5j", default_opts)
+map("v", "<C-k>", "5k", default_opts)
+
+-- move around splits using Ctrl + {h,j,k,l}
+map("n", "<C-h>", "<C-w>h", default_opts)
+map("n", "<C-l>", "<C-w>l", default_opts)
+
+-- find next match
+map("n", "<C-n>", "*", default_opts)
+
+-- close all windows and exit from neovim
+-- map('n', '<leader>q', ':qa!<CR>', default_opts)
+
+-----------------------------------------------------------
+-- Applications & Plugins shortcuts:
+-----------------------------------------------------------
+-- open terminal
+map("n", "<C-t>", ":Term<CR>", { noremap = true })
+
+-- Vista tag-viewer
+map("n", "<C-m>", ":Vista!!<CR>", default_opts) -- open/close
---
+++ neovim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,73 @@
+-----------------------------------------------------------
+-- Autocomplete configuration file
+-----------------------------------------------------------
+
+-- Plugin: nvim-cmp
+-- https://github.com/hrsh7th/nvim-cmpa
+
+local cmp = require "cmp"
+local luasnip = require "luasnip"
+
+cmp.setup {
+  -- load snippet support
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+
+  -- completion settings
+  completion = {
+    --completeopt = 'menu,menuone,noselect'
+    keyword_length = 2,
+  },
+
+  -- key mapping
+  mapping = {
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+
+    -- Tab mapping
+    ["<Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        -- work with copilot
+        -- @see https://github.com/hrsh7th/nvim-cmp/issues/459#issuecomment-957140674
+        local copilot_keys = vim.fn["copilot#Accept"]()
+        if copilot_keys ~= "" then
+          vim.api.nvim_feedkeys(copilot_keys, "i", true)
+        else
+          fallback()
+        end
+      end
+    end,
+    ["<S-Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end,
+  },
+
+  -- load sources, see: https://github.com/topics/nvim-cmp
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "path" },
+    { name = "buffer" },
+  },
+}
