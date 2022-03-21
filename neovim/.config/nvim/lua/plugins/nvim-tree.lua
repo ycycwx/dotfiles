6f614ad90eb2a57c679832941775ae14bab7ca6e
ycycwx <yytcjcy@gmail.com>
2022-03-21 21:10:36 +0800

    feat: update nvim tree config

--- neovim/.config/nvim/lua/plugins/nvim-tree.lua
+++ neovim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -16,11 +16,9 @@ local g = vim.g
 local map = vim.api.nvim_set_keymap
 local default_opts = { noremap = true, silent = true }
 
-g.nvim_tree_quit_on_open = 0
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_highlight_opened_files = 1
-g.nvim_tree_disable_window_picker = 1
 g.nvim_tree_respect_buf_cwd = 1
 g.nvim_tree_width_allow_resize = 1
 g.nvim_tree_show_icons = {
@@ -36,6 +34,12 @@ g.nvim_tree_icons = {
 require('nvim-tree').setup({
   open_on_setup = true,
   update_cwd = true,
+  actions = {
+    open_file = {
+      enable = true,
+      quit_on_open = false,
+    }
+  },
   update_focused_file = {
     enable = true,
     update_cwd = true,
