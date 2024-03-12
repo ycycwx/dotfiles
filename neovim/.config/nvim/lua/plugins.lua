777fdf6e7122d2f0dbda34d3a54fd0db60c2cf83
ycycwx <yytcjcy@gmail.com>
2024-03-12 14:39:47 +0800

    feat: add CopilotChat.nvim

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -271,6 +271,20 @@ lazy.setup({
   },
 
   {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
+    dependencies = {
+      { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+
+  {
     'nvim-lualine/lualine.nvim',
     config = function()
       require('interface.lualine')
