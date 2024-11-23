9ea77163a5f80136cbcc0c671b8410ced464d693
ycycwx <yytcjcy@gmail.com>
2024-11-23 20:10:02 +0800

    chore: upgrade deps

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -277,6 +277,7 @@ lazy.setup({
       { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
       { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
     },
+    build = 'make tiktoken', -- Only on MacOS or Linux
     opts = {
       debug = true, -- Enable debugging
       -- See Configuration section for rest
