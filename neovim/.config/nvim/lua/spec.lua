288057663ddd922de5deddec259cd62520f15644
ycycwx <yytcjcy@gmail.com>
2024-12-26 16:32:04 +0800

    refactor(neovim): migrate markdown

--- neovim/.config/nvim/lua/spec.lua
+++ neovim/.config/nvim/lua/spec.lua
@@ -162,15 +162,6 @@ return {
   },
 
   {
-    'iamcco/markdown-preview.nvim',
-    build = 'cd app && npm install',
-    init = function()
-      vim.g.mkdp_filetypes = { 'markdown' }
-    end,
-    ft = { 'markdown' },
-  },
-
-  {
     'b0o/SchemaStore.nvim',
     config = function()
       require('language.schema-store')
