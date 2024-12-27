b24fd8545ab77f87c8abb06b5538a8b6568a5dd8
ycycwx <yytcjcy@gmail.com>
2024-12-27 23:48:53 +0800

    refactor(neovim): refactor manager

--- neovim/.config/nvim/lua/manager.lua
+++ neovim/.config/nvim/lua/manager.lua
@@ -28,13 +28,6 @@ lazy.setup({
   },
   lockfile = vim.fn.stdpath('config') .. '/lazy-lock.json', -- lockfile generated after running update.
   concurrency = nil, ---@type number limit the maximum amount of concurrent tasks
-  git = {
-    -- defaults for the `Lazy log` command
-    -- log = { "-10" }, -- show the last 10 commits
-    log = { '--since=3 days ago' }, -- show commits from the last 3 days
-    timeout = 120, -- kill processes that take more than 2 minutes
-    url_format = 'https://github.com/%s.git',
-  },
   dev = {
     -- directory where you store your local plugin projects
     path = '~/Documents/code/nvim',
@@ -45,27 +38,13 @@ lazy.setup({
     -- install missing plugins on startup. This doesn't increase startup time.
     missing = true,
     -- try to load one of these colorschemes when starting an installation during startup
-    colorscheme = { 'habamax' },
+    colorscheme = { 'default' },
   },
   ui = {
-    -- a number <1 is a percentage., >1 is a fixed size
-    size = { width = 0.8, height = 0.8 },
-    -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
     border = custom.border,
-    icons = {
-      cmd = ' ',
-      config = '',
-      event = '',
-      ft = ' ',
-      init = ' ',
-      keys = ' ',
-      plugin = ' ',
-      runtime = ' ',
-      source = ' ',
-      start = '',
-      task = '✔ ',
-    },
-    throttle = 20, -- how frequently should the ui process render events
+  },
+  diff = {
+    cmd = 'diffview.nvim',
   },
   checker = {
     -- automatically check for plugin updates
@@ -80,16 +59,6 @@ lazy.setup({
     notify = true, -- get a notification when changes are found
   },
   performance = {
-    cache = {
-      enabled = true,
-      path = vim.fn.stdpath('state') .. '/lazy/cache',
-      -- Once one of the following events triggers, caching will be disabled.
-      -- To cache all modules, set this to `{}`, but that is not recommended.
-      -- The default is to disable on:
-      --  * VimEnter: not useful to cache anything else beyond startup
-      --  * BufReadPre: this will be triggered early when opening a file from the command line directly
-      disable_events = { 'VimEnter', 'BufReadPre' },
-    },
     reset_packpath = true, -- reset the package path to improve startup time
     rtp = {
       reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
@@ -107,15 +76,6 @@ lazy.setup({
         -- "zipPlugin",
       },
     },
-  },
-  -- lazy can generate helptags from the headings in markdown readme files,
-  -- so :help works even for plugins that don't have vim docs.
-  -- when the readme opens with :help it will be correctly displayed as markdown
-  readme = {
-    root = vim.fn.stdpath('state') .. '/lazy/readme',
-    files = { 'README.md' },
-    -- only generate markdown helptags for plugins that dont have docs
-    skip_if_doc_exists = true,
   },
 })
 
