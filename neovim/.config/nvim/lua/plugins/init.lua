2b80a3a22d68c007a9d6f5a89cd0567cad91886c
ycycwx <yytcjcy@gmail.com>
2024-12-29 22:44:03 +0800

    feat(neovim): add nvim-dap for jest & vitest

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -1,9 +1,11 @@
 return {
   { import = 'plugins.colorscheme' },
+  { import = 'plugins.dap' },
   { import = 'plugins.edit' },
   { import = 'plugins.efficiency' },
   { import = 'plugins.filetype' },
   { import = 'plugins.git' },
+  { import = 'plugins.lib' },
   { import = 'plugins.lsp' },
   { import = 'plugins.tool' },
   { import = 'plugins.treesitter' },
