e12a6a4b3cd528b85cb86e4a90e059b7b8afde5d
ycycwx <yytcjcy@gmail.com>
2025-03-25 19:25:25 +0800

    fix(neovim): fix neovim type definition

--- neovim/.config/nvim/lua/plugins/lsp/mason-lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/lsp/mason-lspconfig.lua
@@ -45,6 +45,14 @@ return {
                   showWord = 'Disable',
                   workspaceWord = false,
                 },
+                workspace = {
+                  library = {
+                    vim.env.VIMRUNTIME,
+                    -- -- Depending on the usage, you might want to add additional paths here.
+                    '${3rd}/luv/library',
+                    '${3rd}/busted/library',
+                  },
+                },
               },
             },
           })
