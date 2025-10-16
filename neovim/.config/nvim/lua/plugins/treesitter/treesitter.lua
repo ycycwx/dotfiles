e56b9851c7ac728ee7fb93338d1d7a6fa5a48bf6
ycycwx <yytcjcy@gmail.com>
2025-10-16 23:18:41 +0800

    chore: upgrade treesitter config

--- neovim/.config/nvim/lua/plugins/treesitter/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter/treesitter.lua
@@ -1,18 +1,23 @@
+---@type LazyPluginSpec
 return {
   'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
   event = 'VeryLazy',
   build = ':TSUpdate',
   opts = {
+    ---@diagnostic disable-next-line: param-type-mismatch
     parser_install_dir = vim.fs.joinpath(vim.fn.stdpath('data'), 'site'),
     ensure_installed = 'all',
     highlight = {
       enable = true,
+      -- Should be controlled by vimtex
+      disable = { 'latex' },
     },
     indent = {
       enable = true,
     },
   },
   config = function(_, opts)
-    require('nvim-treesitter.configs').setup(opts)
+    require('nvim-treesitter').setup(opts)
   end,
 }
