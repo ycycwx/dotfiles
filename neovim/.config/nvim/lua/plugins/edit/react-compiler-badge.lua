814ffbe0b8f2b9752883e355599a0707c484ad99
ycycwx <yytcjcy@gmail.com>
2025-12-06 21:49:55 +0800

    fix(neovim): adjust start event of react-compiler-badge

--- neovim/.config/nvim/lua/plugins/edit/react-compiler-badge.lua
+++ neovim/.config/nvim/lua/plugins/edit/react-compiler-badge.lua
@@ -2,7 +2,9 @@ return {
   'ycycwx/react-compiler-badge.nvim',
   build = 'npm install', -- Installs dependencies for analyzer.js
   event = {
+    'BufReadPost *.ts',
     'BufReadPost *.tsx',
+    'BufReadPost *.js',
     'BufReadPost *.jsx',
   },
 }
