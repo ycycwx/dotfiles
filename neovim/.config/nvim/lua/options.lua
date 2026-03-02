6b5d0304f60e837003e7a9c4e6836a5dc38b6eec
ycycwx <yytcjcy@gmail.com>
2026-03-03 00:17:49 +0800

    refactor: use mixed vim.o and vim.opt for cleaner options.lua

--- neovim/.config/nvim/lua/options.lua
+++ neovim/.config/nvim/lua/options.lua
@@ -1,40 +1,42 @@
-vim.opt.exrc = true
+vim.o.exrc = true
 
 -- Edit
-vim.opt.expandtab = true
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.undofile = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.list = true
-vim.opt.includeexpr = "substitute(v:fname,'\\.','/','g')"
-vim.opt.jumpoptions = 'stack'
-vim.opt.completeopt = { 'menu', 'menuone', 'noinsert' }
-vim.opt.swapfile = false
-vim.opt.clipboard = 'unnamedplus' -- copy/paste to system clipboard
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.list = true
+vim.o.includeexpr = "substitute(v:fname,'\\.','/','g')"
+vim.o.jumpoptions = 'stack'
+vim.o.swapfile = false
+vim.o.clipboard = 'unnamedplus' -- copy/paste to system clipboard
 
 -- Interface
-vim.opt.confirm = true
-vim.opt.splitkeep = 'screen'
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.number = true
-vim.opt.breakindent = true
-vim.opt.linebreak = true
-vim.opt.mouse = 'a'
-vim.opt.mousemoveevent = true
-vim.opt.termguicolors = true
-vim.opt.title = true
-vim.opt.signcolumn = 'yes'
-vim.opt.cursorline = true
-vim.opt.conceallevel = 2
-vim.opt.scrolloff = 2
-vim.opt.sidescrolloff = 5
-vim.opt.smoothscroll = true
-vim.opt.pumblend = 12
-vim.opt.pumheight = 12
-vim.opt.guifont = 'Cascadia Code NF:h12'
+vim.o.confirm = true
+vim.o.splitkeep = 'screen'
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.number = true
+vim.o.breakindent = true
+vim.o.linebreak = true
+vim.o.mouse = 'a'
+vim.o.mousemoveevent = true
+vim.o.termguicolors = true
+vim.o.title = true
+vim.o.signcolumn = 'yes'
+vim.o.cursorline = true
+vim.o.conceallevel = 2
+vim.o.scrolloff = 2
+vim.o.sidescrolloff = 5
+vim.o.smoothscroll = true
+vim.o.pumblend = 12
+vim.o.pumheight = 12
+vim.o.guifont = 'Cascadia Code NF:h12'
+
+-- Use vim.opt for complex types and methods to keep code clean
+vim.opt.completeopt = { 'menu', 'menuone', 'noinsert' }
 vim.opt.shortmess:append('I')
 vim.opt.fillchars = {
   eob = ' ',
