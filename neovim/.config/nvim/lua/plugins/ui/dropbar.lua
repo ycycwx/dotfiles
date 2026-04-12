7312a80a2bb8c1564d3a0611054e0d03a6bd9090
ycycwx <yytcjcy@gmail.com>
2026-04-12 20:17:05 +0800

    chore: update deps

--- neovim/.config/nvim/lua/plugins/ui/dropbar.lua
+++ neovim/.config/nvim/lua/plugins/ui/dropbar.lua
@@ -7,6 +7,17 @@ return {
     'BufNewFile',
   },
   opts = {
+    bar = {
+      update_events = {
+        buf = {
+          'BufModifiedSet',
+          'FileChangedShellPost',
+          'TextChanged',
+          'ModeChanged',
+          'TextChangedT',
+        },
+      },
+    },
     icons = {
       kinds = {
         symbols = vim.tbl_extend('keep', { Folder = ' ' }, custom.icons.kind_with_space),
@@ -21,6 +32,11 @@ return {
           })
         end,
       },
+    },
+    terminal = {
+      name = function(buf)
+        return vim.b[buf].term_title or vim.fn.bufname(buf)
+      end,
     },
   },
 }
